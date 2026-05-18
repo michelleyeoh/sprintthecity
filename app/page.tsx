83dd7b45b1e912c8ca5a51d6d88859de9ef9f79a
michelleyeoh <74385095+michelleyeoh@users.noreply.github.com>
2026-05-17 20:30:20 -0700

    feat: updated metadata and added prettier (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,34 @@
-'use client'
-import { Typewriter } from 'react-simple-typewriter'
+"use client";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function Home() {
   return (
     <div className="bg-[#F7F7F7] min-h-screen">
       <main className="flex flex-col items-center justify-center h-screen">
         <div className="text-left max-w-[550px]">
-          <h1 className="font-serif text-black text-6xl pb-2">Sprint the City ✈︎</h1>
+          <h1 className="font-serif text-black text-6xl pb-2">
+            Sprint the City ✈︎
+          </h1>
           <h1 className="font-serif text-black text-4xl">
             How much of{" "}
-            <span style={{ color: '#5b0f00', fontWeight: 'bold' }}>
-            <Typewriter
-              words={['Los Angeles', 'San Diego', 'Seattle', 'San Francisco', 'New York City']}
-              loop={5}
-              cursor
-              cursorStyle='|'
-              typeSpeed={100}
-              deleteSpeed={80}
-              delaySpeed={2000}
-            />
-          </span>
-          <br/>
+            <span style={{ color: "#5b0f00", fontWeight: "bold" }}>
+              <Typewriter
+                words={[
+                  "Los Angeles",
+                  "San Diego",
+                  "Seattle",
+                  "San Francisco",
+                  "New York City",
+                ]}
+                loop={5}
+                cursor
+                cursorStyle="|"
+                typeSpeed={100}
+                deleteSpeed={80}
+                delaySpeed={2000}
+              />
+            </span>
+            <br />
             have you explored?
           </h1>
           {/* <p className="font-serif text-black">Login today to find out!</p> */}
