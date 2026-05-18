973d8b3b9c52bdd56e36ae979a011ff3c99e1125
michelleyeoh <michellew.yeoh@gmail.com>
2026-05-18 13:33:22 -0700

    force dynamic skip for /sprint page

--- app/sprint/page.tsx
+++ app/sprint/page.tsx
@@ -1,5 +1,7 @@
 import SprintBoard from "./_components/sprint";
 
+export const dynamic = "force-dynamic";
+
 export default function Sprint() {
   return (
     <div className="bg-[#FFFFFF] min-h-screen">
